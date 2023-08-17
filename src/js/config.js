7a6399637672a42b7db1c6bad0a8ef6dd803f162
Loi Phan <phanhuuloi15@gmail.com>
2023-08-17 11:42:17 +0700

    Finish Project

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const API_KEY = 'aa3a2565-6eea-42a4-bf0a-cb0077fce6cc';
+export const MODAL_CLOSE_SEC = 2.5;
